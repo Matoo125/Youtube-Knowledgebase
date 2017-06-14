99e82c3e58bf10a154305fb628fd8d7d60695156
Matej <vrzala.matej@gmail.com>
2017-06-14 19:13:38 +0200

    1

--- app/model/Channel.php
+++ app/model/Channel.php
@@ -34,7 +34,7 @@ class Channel extends Model
 	{
 		$query = $this->query->select($items)
 		                     ->from(self::$table)
-		                     ->orderBy('published_at', 'DESC')
+		                     ->orderBy('created_at', 'DESC')
 		                     ->build();
 
 		return $this->fetchAll($query, []);
